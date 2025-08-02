f68546af795054845d141981f94f6e9fe81caf96
chattermate <admin@chattermate.chat>
2025-08-02 11:25:54 +0530

    fixes frontend type check

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -14,7 +14,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(VueApexCharts)
+app.component('apexchart', VueApexCharts)
 app.use(FloatingVue)
 
 initializeFirebase()
